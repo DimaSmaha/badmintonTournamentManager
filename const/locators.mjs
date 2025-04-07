37a01119d2796c5952d6cd6e525832ce74df6d0a
DimaSmaha <dmytrosmaha@gmail.com>
2025-04-07 18:08:40 +0300

    upd

---
+++ const/locators.mjs
@@ -0,0 +1,13 @@
+class Locators {
+  constructor() {
+    this.playersInputBox = document.querySelector("#playersInputBox");
+    this.playersNumberCounter = document.querySelector("#playersNumber");
+    this.addPlayer = document.querySelector("#addPlayer");
+    this.subtractPlayer = document.querySelector("#subtractPlayer");
+    this.canNotBeEmpty = document.querySelector("#canNotBeEmpty");
+    this.notLessThan2 = document.querySelector("#notLessThan2");
+    this.notMoreThan256 = document.querySelector("#notMoreThan256");
+  }
+}
+
+export default new Locators();
--- modules/playerInputs.mjs
+++ modules/playerInputs.mjs
@@ -19,3 +19,33 @@ export function generateInitialInputs() {
   generatePlayerInput();
   updatePlayersNumber();
 }
+
+locators.addPlayer.addEventListener("click", () => {
+  addNewPlayerInput();
+});
+
+locators.subtractPlayer.addEventListener("click", () => {
+  removePlayerInput();
+});
+
+function addNewPlayerInput() {
+  //set no more then 256
+  generatePlayerInput();
+  updatePlayersNumber();
+}
+
+function removePlayerInput() {
+  const getInputNum = locators.playersInputBox.childElementCount;
+  const getLastInput = locators.playersInputBox.children[getInputNum - 1];
+  if (getInputNum <= 2) {
+    console.log("can not delete");
+    setTimeout(() => {
+      //   locators.notLessThan2.st; // write errors
+    }, 3000);
+  }
+
+  if (getInputNum > 2) {
+    getLastInput.remove();
+    updatePlayersNumber();
+  }
+}
